e99f966e1955aade899842453abe5084cd8549c6
correct_username <ninaananda@gmail.com>
2024-12-28 22:04:18 +0100

    marmar

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codebind <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 16:37:59 by codebind          #+#    #+#             */
+/*   Updated: 2024/12/28 22:03:30 by codebind         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	lenght;
+
+	i = 0;
+	lenght = 0;
+	while (src[lenght] != '\0')
+		lenght++;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dest[i] = '\0';
+	return (lenght);
+}
