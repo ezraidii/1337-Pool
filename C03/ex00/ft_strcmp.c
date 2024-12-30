9e7c49cd9813ee36788941d36340e83ae98b922e
MARIAMA <maramzraydi@gmail.com>
2024-12-30 19:00:53 +0000

    ft_strcmp.c

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -10,16 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(char *s1, char *s2)
+int     ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 != *s2)
-		{
-		return (unsigned char)(*s1) - (unsigned char)(*s2);
-		}
-        	s1++;
-        	s2++;
-    	}
-	return (unsigned char)(*s1) - (unsigned char)(*s2);
+        while (*s1 != '\0' && *s2 != '\0')
+        {
+                if (*s1 != *s2)
+                {
+                        return ((unsigned char)(*s1) - (unsigned char)(*s2));
+                }
+                s1++;
+                s2++;
+        }
+        return ((unsigned char)(*s1) - (unsigned char)(*s2));
 }
