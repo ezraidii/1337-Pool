6563c97daf6e1730ec9183c686d1aea04e3cc227
correct_username <ninaananda@gmail.com>
2024-12-31 15:59:37 +0100

    marmar

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: codebind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/31 13:20:34 by codebind          #+#    #+#             */
-/*   Updated: 2024/12/31 13:55:34 by codebind         ###   ########.fr       */
+/*   Created: 2024/12/31 14:29:47 by codebind          #+#    #+#             */
+/*   Updated: 2024/12/31 14:50:04 by codebind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0')) 
-	{
-        	if (s1[i] != s2[i])
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
-		i++;
-	}
-	return 0;
+    i = 0;
+    while (i < n && (s1[i] != '\0'|| s2[i] != '\0')) 
+    {
+        if (s1[i] != s2[i])
+        {
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        }
+        i++;
+    }
+    return 0;
 }
