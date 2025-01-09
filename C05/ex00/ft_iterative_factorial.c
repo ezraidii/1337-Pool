94bb677851aa7ed7551f561947aa46cebb785072
MARIAMA <maramzraydi@gmail.com>
2025-01-09 12:49:23 +0000

    ft_iterative_factorial.c

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -10,21 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int     ft_iterative_factorial(int nb)
 {
-	int	i;
-	int	factoriel;
+        int     factoriel;
 
-	if (nb < 0)
-	{
-		return 0;
-	}
-	i = 1;
-	factoriel = 1;
-	while (i <= nb)
-	{
-		factoriel *= i;
-		i++;
-	}
-	return factoriel;
+        if (nb < 0)
+        {
+                return 0;
+        }
+        factoriel = 1;
+        while (nb >= 1)
+        {
+                factoriel *= nb;
+                nb--;
+        }
+        return factoriel;
 }
